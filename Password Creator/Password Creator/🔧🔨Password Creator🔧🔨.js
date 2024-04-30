d2ee7f0e3ac4d2232e48659ca0fb3a01889b431f
anthony2356 <157649718+anthony2356@users.noreply.github.com>
2024-04-29 22:41:16 -0300

    Add files via upload

---
+++ Password Creator/Password Creator/🔧🔨Password Creator🔧🔨.js
@@ -0,0 +1,506 @@
+// Messages popPup Area \\
+
+function message3() {
+  alert("Password creator developed by: anthony2356");
+}
+
+function message2() {
+  alert("UPDATE: WINDOWS CMD PROMPT FOR PASSWORD CREATED !");
+  alert("UPDATE: BUG FIXES! [CSS ISSUE] & TWO NEW BUTTONS !");
+  alert("UPDATE: JAVASCRIPT SOURCE CODE RE-WRITE & SOME BUTTONS REMOVED !");
+  alert("UPDATE: ADDED MINECRAFT TEXTURES AND BACKGROUND !");
+  alert("SOON: NEW PASSWORDS FOR THE PASSWORD CREATOR !");
+}
+
+function mensagem() {
+  alert(
+    "you can put anything in the 4 inputs, but, if you wanna go on the conventional way, you put the data as the info says"
+  );
+  alert("your password will be created on the windows cmd prompt");
+}
+
+// Cleaning Area \\
+
+function limpar_dados() {
+  document.getElementById("uppercase").value = "";
+  document.getElementById("lowercase").value = "";
+  document.getElementById("numbers").value = "";
+  document.getElementById("simbols").value = "";
+  alert("removing all inserted data in all the 4 inputs.");
+}
+
+function limpar_senha() {
+  alert("deleting created password... ");
+  document.getElementById("password-created").value = "";
+  alert("password deleted !");
+}
+
+// Password Area \\
+
+function gs() {
+  n = document.getElementById("numbers").value;
+  u = document.getElementById("uppercase").value;
+  l = document.getElementById("lowercase").value;
+  s = document.getElementById("simbols").value;
+  pc = document.getElementById("password-created").value =
+    u + s + n + l + u + s + l;
+  v();
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+gs();
+
+function mm() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l4;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function im() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l6;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function gm() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  l7 = l6 + Math.random(0, 1) + l1;
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l7;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function fbim() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  l7 = l6 + Math.random(0, 1) + l1;
+  l8 =
+    l7 +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1);
+  l9 = l8 + l8 * 13287941248099132;
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l9;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function cm() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  l7 = l6 + Math.random(0, 1) + l1;
+  l8 =
+    l7 +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1);
+  l9 = l8 + l8 * 13287941248099132;
+  l10 = l9 * Math.random(0, 1) + l8 + l1;
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l10;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function bm() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  l7 = l6 + Math.random(0, 1) + l1;
+  l8 =
+    l7 +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1);
+  l9 = l8 + l8 * 13287941248099132;
+  l10 = l9 * Math.random(0, 1) + l8 + l1;
+  l11 = l10 + l10 * l8 + Math.random(0, 1) + Math.random(0, 1);
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l11;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+function bgm() {
+  l1 = "0101010110110110101010101";
+  l2 = Math.random(0, 1);
+  l3 = l2 + l1;
+  l4 = l3 + l3 + 2;
+  l5 = l4 + Math.random(0, 1);
+  l6 = l5 + l5;
+  l7 = l6 + Math.random(0, 1) + l1;
+  l8 =
+    l7 +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1) +
+    Math.random(0, 1);
+  l9 = l8 + l8 * 13287941248099132;
+  l10 = l9 * Math.random(0, 1) + l8 + l1;
+  l11 = l10 + l10 * l8 + Math.random(0, 1) + Math.random(0, 1);
+  l12 = l11 + l11 * l8 + l1 + Math.random(0, 1);
+  l14 = l3 + l12 + Math.random(0, 1);
+  v();
+  gs();
+  function gs() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l + l11;
+  }
+  function v() {
+    n = document.getElementById("numbers").value;
+    u = document.getElementById("uppercase").value;
+    l = document.getElementById("lowercase").value;
+    s = document.getElementById("simbols").value;
+    pc = document.getElementById("password-created").value =
+      u + s + n + l + u + s + l;
+    function limpar_senha() {
+      alert("deleting created password... ");
+      document.getElementById("password-created").value = "";
+      alert("password deleted !");
+    }
+    function limpar_dados() {
+      document.getElementById("uppercase").value = "";
+      document.getElementById("lowercase").value = "";
+      document.getElementById("numbers").value = "";
+      document.getElementById("simbols").value = "";
+      alert("removing all inserted data in all the 4 inputs.");
+    }
+    if (pc == "") {
+      alert("nada inserido, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    } else if (pc == pc) {
+      alert("nenhum erro");
+    } else {
+      alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+      limpar_senha();
+      limpar_dados();
+    }
+  }
+}
+
+// Validation Area
+
+function v() {
+  n = document.getElementById("numbers").value;
+  u = document.getElementById("uppercase").value;
+  l = document.getElementById("lowercase").value;
+  s = document.getElementById("simbols").value;
+  pc = document.getElementById("password-created").value =
+    u + s + n + l + u + s + l;
+  function limpar_senha() {
+    alert("deleting created password... ");
+    document.getElementById("password-created").value = "";
+    alert("password deleted !");
+  }
+  function limpar_dados() {
+    document.getElementById("uppercase").value = "";
+    document.getElementById("lowercase").value = "";
+    document.getElementById("numbers").value = "";
+    document.getElementById("simbols").value = "";
+    alert("removing all inserted data in all the 4 inputs.");
+  }
+  if (pc == "") {
+    alert("nada inserido, ativando protocolo de limpeza");
+    limpar_senha();
+    limpar_dados();
+  } else if (pc == pc) {
+    alert("nenhum erro");
+  } else {
+    alert("tentativa de burlar o sistema, ativando protocolo de limpeza");
+    limpar_senha();
+    limpar_dados();
+  }
+}
